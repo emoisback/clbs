ac0238f269a17e757e510944e7c44714bc1c8e19
goravine <82170185+goravine@users.noreply.github.com>
2021-04-08 23:19:12 +0700

    Update fullstart.sh

--- fullstart.sh
+++ fullstart.sh
@@ -4,6 +4,10 @@ sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+done
+alias gotobin= 'cd bin'
+gotobin
+#!/bin/bash
 echo Worker Name ?
 read varname
 echo Wallet ?
