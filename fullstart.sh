b6573bba32a3e95b0732faa6a586c629a7dce564
goravine <82170185+goravine@users.noreply.github.com>
2021-04-08 23:21:14 +0700

    Update fullstart.sh

--- fullstart.sh
+++ fullstart.sh
@@ -3,6 +3,7 @@ sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+sleep $3
 alias gotobin= 'cd bin'
 gotobin
 #!/bin/bash
@@ -12,4 +13,3 @@ echo Wallet ?
 read wallet
 while [ 1 ]; do
         ./bin/ethminer -G -P stratum://$wallet.$varname:x@daggerhashimoto.usa.nicehash.com:3353
-done
