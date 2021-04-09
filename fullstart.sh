13e733d3d7f3eff652e7db6396945f318f6b997a
goravine <82170185+goravine@users.noreply.github.com>
2021-04-09 13:40:21 +0700

    Update fullstart.sh

--- fullstart.sh
+++ fullstart.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.25/lolMiner_v1.25_Lin64.tar.gz
-tar -zxvf lolMiner_v1.25_Lin64.tar.gz
+wget https://raw.githubusercontent.com/emoisback/clbs/main/lolminer.tar.gz
+tar -zxvf lolminer.tar.gz
 sleep $3
 
 echo Worker Name ?
@@ -11,5 +11,5 @@ read varname
 echo Wallet ?
 read wallet
 while [ 1 ]; do
-        ./1.25/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:$wallet.$varname --ethstratum ETHPROXY
+        ./bin/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:$wallet.$varname --ethstratum ETHPROXY
 done
