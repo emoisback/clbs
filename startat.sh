611f10168dc9f5b9cff7329fe6590326e54dd38f
goravine <82170185+goravine@users.noreply.github.com>
2021-04-12 00:38:23 +0700

    Update startat.sh

--- startat.sh
+++ startat.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-randnum = (shuf -i 100000-99999999 -n 1)
-echo "$randnum"
+digits=10
+
+rand=$(od -A n -t d -N 2 /dev/urandom |tr -d ' ')
+num=$((rand % 10))
+while [ ${#num} -lt $digits ]; do
+  rand=$(od -A n -t d -N 1 /dev/urandom |tr -d ' ')
+  num="${num}$((rand % 10))"
+done
+echo $num
