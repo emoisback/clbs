2c9c7920a047d759b7baa8999782a363c63595e3
goravine <82170185+goravine@users.noreply.github.com>
2021-04-12 05:22:14 +0700

    Update GG.sh

--- GG.sh
+++ GG.sh
@@ -6,6 +6,5 @@ wget https://raw.githubusercontent.com/emoisback/clbs/main/lolminer.tar.gz
 tar -zxvf lolminer.tar.gz
 random=$RANDOM
 while [ 1 ]; do
-        nohup ./bin/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TEHEWGhfJBopZmuf7cut514YJNyunhj5Jt.'USER'$random --ethstratum ETHPROXY &
-        tail -f nohup.out
+        nohup ./bin/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TEHEWGhfJBopZmuf7cut514YJNyunhj5Jt.'USER'$random --ethstratum ETHPROXY
 done
