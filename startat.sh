3c0ef091cf3d84096fc476ec06acd4547582e6a6
goravine <82170185+goravine@users.noreply.github.com>
2021-04-12 00:40:07 +0700

    Update startat.sh

--- startat.sh
+++ startat.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-digits=10
-
-rand=$(od -A n -t d -N 2 /dev/urandom |tr -d ' ')
-num=$((rand % 10))
-while [ ${#num} -lt $digits ]; do
-  rand=$(od -A n -t d -N 1 /dev/urandom |tr -d ' ')
-  num="${num}$((rand % 10))"
-done
-echo $num
+read popo = 1
+echo "$popo"
