98dea5623d8e3992167ca3b0e83daca051bcbd3a
Nagataka <irsyadpso2@gmail.com>
2021-05-19 10:32:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
-tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
+tar -zxvf t-rex-0.20.3-linux.tar.gz
 cd bin
