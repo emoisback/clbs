0c7c6eb6eaaa1b61e975a1624d362592d32c4c4c
Nagataka <irsyadpso2@gmail.com>
2021-05-19 10:33:04 +0800

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
-tar -zxvf t-rex-0.19.14-linux-cuda11.1.tar.gz
+tar -zxvf t-rex-0.20.3-linux.tar.gz
 cd bin
